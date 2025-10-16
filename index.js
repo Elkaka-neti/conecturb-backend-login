1641e04af254f3a92ece8a0b976d67d451ede300
Kauã Neves <73667960+Elkaka-neti@users.noreply.github.com>
2025-10-16 14:48:00 -0300

    [UPDATE] Adicionando lógica de rotas para index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const registerRoutes = require('./utils/registerRouters');
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+        res.send("hello");
+    });  
+
+const routerDir = path.join(__dirname, 'routers');
+    registerRoutes(app, routerDir);
+
+app.listen(3000, '127.0.0.1', () => {
+  console.log('Servidor rodando na porta 3000');
+});
+
+module.exports = app;
